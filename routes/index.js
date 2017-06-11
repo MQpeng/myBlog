835d91176330d3ae6f38d832e5f18bc80c1fd7e3
MQpeng <742486126@qq.com>
2017-06-11 15:49:55 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,7 @@ var Article = Parse.Object.extend("Articles");
 // 递归解决异步无法渲染的问题
 var query = new Parse.Query(Article);
 var articles = [];
-query.addDescending("createAt");
-query.find().then((data) => {
+query.addDescending("createdAt").find().then((data) => {
   // console.log("data", data)
   data.forEach(article => {
     articles.push(article.toJSON())
@@ -20,8 +19,7 @@ query.find().then((data) => {
   // console.log('articles', articles)
   return router.get('/', function(req, res, next) {
     var query = new Parse.Query(Article);
-    query.addDescending("createAt");
-    query.find().then((data) => {
+    query.addDescending("createdAt").find().then((data) => {
       if (articles.length != data.length) {
         articles = [];
         data.forEach(article => {
